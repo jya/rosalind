7fa44bf9b82197ce5d674880d226bc6781afc64c
Jason <jy2an@virginia.edu>
2016-08-31 11:58:08 -0700

    updates 8/31

--- strong_bioinf/lcsm.py
+++ strong_bioinf/lcsm.py
@@ -2,4 +2,23 @@ from parse import *
 
 inf = open('/home/jason/Downloads/rosalind_lcsm.txt')
 seqs = parse(inf)
+seqs = seqs.values()
 
+def is_substr(find, data):
+	if len(data) < 1 and len(find) < 1:
+		return False
+	for i in range(len(data)):
+		if find not in data[i]:
+			return False
+	return True
+
+def longest_substr(data):
+	longest = ''
+	if len(data) > 1 and len(data[0]) > 0:
+		for i in range(len(data[0])):
+			for j in range(len(data[0])-i+1):
+				if j > len(longest) and is_substr(data[0][i:i+j], data):
+					longest = data[0][i:i+j]
+	return longest
+
+print longest_substr(seqs)
---
+++ strong_bioinf/lia.py
@@ -0,0 +1,3 @@
+inf = open('/home/jason/Downloads/rosalind_lia.txt')
+input = map(int,inf.readline().strip().split())
+
